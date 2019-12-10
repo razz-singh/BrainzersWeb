242583bb52d8f5c8ee71c699e8d249be43b7d744
Razz <ebsadmin@Amits-MacBook-Pro.local>
2019-12-10 19:47:44 +0530

    Bootstrap css added
    Carousel added with picsome images  using ng bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Welcome To Brainzers';
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/1920/780`);
 }
